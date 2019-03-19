4f8ef9181054006bf2c388a05ba6c5246f395a59
Brandon Buttlar <brandonbuttlar@mail.missouri.edu>
2019-03-19 16:33:56 -0500

    First commit

---
+++ DocumentMerger2/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace DocumentMerger2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("");
+
+            Console.ReadLine();
+        }
+    }
+}
